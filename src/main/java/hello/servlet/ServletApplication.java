972b559a4a511efb533341b1fc8143c1c85bf586
seunghyo <tmdgy6232@naver.com>
2023-11-08 22:21:05 +0900

    create: http Servlet 기본 사용법 in spring boot

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// 서블릿 찾아서 등록하게 해줌(자동등록)
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
